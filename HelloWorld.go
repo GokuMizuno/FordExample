1fae834a3c19096fa2189a9479dc5b2eb9f2135b
GokuMizuno <gordon.stangler@gmail.com>
2019-01-14 16:27:23 -0500

    Create HelloWorld.go
    Hello World

---
+++ HelloWorld.go
@@ -0,0 +1,14 @@
+//Gordon Stangler
+//Hello World, because, well, everyone needs a hello world in their life
+package main
+
+import(
+	"fmt"
+}
+
+func main(){
+	fmt.Println("Hello World!")
+
+	//One can also use:
+	fmt.Printf("Hello World!\n");
+}
