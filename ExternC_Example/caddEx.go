83f70c6f3225096c866aa523dd2031f4ec1e012d
GokuMizuno <gordon.stangler@gmail.com>
2019-01-15 10:01:57 -0500

    Update caddEx.go
    import "C" was needed.
    Note, this is not documented in the golang language docs, as best I can see.

--- ExternC_Example/caddEx.go
+++ ExternC_Example/caddEx.go
@@ -1,8 +1,10 @@
 //Gordon Stangler
 //Goroutines, with a dash of extern C
+//C function is in cadd.c
 
 package main
 
+import "C"
 import(
 	"fmt"
 )
@@ -11,8 +13,6 @@ import(
 func Cdisp(sentArray string) string
 
 func coarrayEX(sentArray string){
-//	sentArray []string = "hello world"
-
 	//extern C
 	returnedArray := Cdisp(sentArray)
 	fmt.Printf("%s\n", returnedArray)
